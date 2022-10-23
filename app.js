b9e2bae6732c818c200049b2fa0d53c109fac32a
Octitrusendi <octaviotrusendi@gmail.com>
2022-10-23 16:04:28 -0300

    estructura express

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+app.use(express.static(__dirname + '/public'));
+
+app.listen(3000, ()=>{
+        console.log('Servidor corriendo');
+});
+
+
+
+app.get("/", (req, res) =>{
+        res.sendFile(path.resolve('./views/index.html'))
+});
