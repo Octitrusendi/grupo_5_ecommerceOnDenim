2b25ccb15dd3763d70877ba4c6910c3999e380ee
Octitrusendi <octaviotrusendi@gmail.com>
2022-11-08 22:26:57 -0300

    cambios

--- appOnDenim/controllers/mainController.js
+++ appOnDenim/controllers/mainController.js
@@ -5,20 +5,16 @@ var createError = require('http-errors');
 const productsFilePath = path.join(__dirname, '../data/productsDataBase.json');
 const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
 
-
-console.log(products)
 const mainController = {
   index: (req, res) => {
-    res.render('index', {products, title: 'OnDenim | Home' });
-
+    res.render('index', { products, title: 'OnDenim | Home' });
   },
   login: (req, res) => {
     res.render('login', { title: 'OnDenim | Login' });
   },
   register: (req, res) => {
     res.render('register', { title: 'OnDenim | Registro' });
-  }
-  
+  },
 };
 
 module.exports = mainController;
