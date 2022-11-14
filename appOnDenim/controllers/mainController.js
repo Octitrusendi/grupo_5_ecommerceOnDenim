1ec9a287d4b7952f7ad029b50dbe1c056bf0af2b
Octitrusendi <octaviotrusendi@gmail.com>
2022-11-13 21:49:59 -0300

    rutas

--- appOnDenim/controllers/mainController.js
+++ appOnDenim/controllers/mainController.js
@@ -4,10 +4,15 @@ var createError = require('http-errors');
 
 const productsFilePath = path.join(__dirname, '../data/productsDataBase.json');
 const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
+const toThousand = n => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, '.');
 
 const mainController = {
   index: (req, res) => {
-    res.render('index', { products, title: 'OnDenim | Home' });
+    res.render('index', {
+      products,
+      toThousand,
+      title: 'OnDenim | Home',
+    });
   },
   login: (req, res) => {
     res.render('login', { title: 'OnDenim | Login' });
@@ -15,8 +20,18 @@ const mainController = {
   register: (req, res) => {
     res.render('register', { title: 'OnDenim | Registro' });
   },
-  totalProductos: (req, res) =>{
-     res.render('totalProductos', { products, title: 'OnDenim | Todos los Jeans' }, );
+  totalProductos: (req, res) => {
+    res.render('totalProductos', {
+      products,
+      toThousand,
+      title: 'OnDenim | Todos los Jeans',
+    });
+  },
+  carrito: (req, res) => {
+    res.render('productCart', {
+      products,
+      title: 'OnDenim | Tu carrito',
+    });
   },
 };
 
