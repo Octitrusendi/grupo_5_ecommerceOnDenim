25e063fe2864f267697923c94c2661a3d207dada
Octitrusendi <octaviotrusendi@gmail.com>
2023-02-20 17:01:01 -0300

    error en carrito

--- appOnDenim/public/javascripts/cart.js
+++ appOnDenim/public/javascripts/cart.js
@@ -20,10 +20,10 @@ function removeItem(index) {
 
 function setCarritoVacio() {
   cartRows.innerHTML = `
-        <tr>     
-            <td colspan="5"><div class="alert alert-warning my-2 text-center">No tienes products en el carrito</div></td>
-        </tr>            
-        `;
+  <tr>     
+      <td colspan="5"><div class="alert alert-warning my-2 text-center">No tienes productos en el carrito</div></td>
+  </tr>            
+  `;
 }
 function vaciarCarrito() {
   localStorage.removeItem('carrito');
@@ -40,7 +40,7 @@ let cartRows = document.querySelector('.cartRows');
 let products = [];
 
 if (localStorage.carrito && localStorage.carrito != '[]') {
-  let carrito = JSON.parse(localStorage.carrito);
+  carrito = JSON.parse(localStorage.carrito);
   carrito.forEach((item, index) => {
     fetch(`/api/products/${item.id}`)
       .then(res => res.json())
@@ -48,18 +48,18 @@ if (localStorage.carrito && localStorage.carrito != '[]') {
         if (product) {
           let finalPrice = product.price - (product.price * product.sale) / 100;
           cartRows.innerHTML += `
-                        <tr id="row${index}">
-                            <th scope="row">${index + 1}</th>
-                            <td>${product.name}</td>
-                            <td>$ ${finalPrice}</td>
-                            <td class="text-center">${item.quantity}</td>
-                            <td class="text-center">$ ${parseFloat(
-                              finalPrice * item.quantity,
-                              2,
-                            ).toFixed(2)}</td>
-                            <td><button class="btn btn-danger btn-sm" onclick=removeItem(${index})><i class="fas fa-trash"></i></button></td>
-                        </tr>            
-                        `;
+          <tr id="row${index}">
+              <th scope="row">${index + 1}</th>
+              <td>${product.name}</td>
+              <td>$ ${finalPrice}</td>
+              <td class="text-center">${item.quantity}</td>
+              <td class="text-center">$ ${parseFloat(
+                finalPrice * item.quantity,
+                2,
+              ).toFixed(2)}</td>
+              <td><button class="btn btn-danger btn-sm" onclick=removeItem(${index})><i class="fas fa-trash"></i></button></td>
+          </tr>            
+          `;
           products.push({
             productId: product.id,
             name: product.name,
@@ -82,7 +82,6 @@ if (localStorage.carrito && localStorage.carrito != '[]') {
 }
 
 let formCheckout = document.querySelector('#checkoutCart');
-let btnComptra = document.querySelector('.btncompra.disabled');
 
 formCheckout.onsubmit = e => {
   e.preventDefault();
@@ -103,9 +102,9 @@ formCheckout.onsubmit = e => {
     .then(res => {
       if (res.ok) {
         vaciarCarrito();
-/*         btnCompra.disabled = true; */
         location.href = `/user/order/${res.order.id}?creado=true`;
       } else {
       }
-    });
+    })
+    .catch(error => console.log(error));
 };
